cdff5f6b5b4cd482a475dcff51f698dc3070f910
Wolfang Torres <wolfang.torres@gmail.com>
2023-02-15 11:42:34 -0400

    update to add poetry

---
+++ src/sistema_indicadores/__init__.py
@@ -0,0 +1,13 @@
+"""__init__.py
+
+Application starting point
+"""
+
+import importlib.metadata
+
+_DISTRIBUTION_METADATA = importlib.metadata.metadata('MyProjectName')
+
+print("hello world!")
+print("Data:")
+for key, val in _DISTRIBUTION_METADATA.items():
+    print(f"\t{key}\t= {val}")
