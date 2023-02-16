c38b4717989838c788121a1b03356e1386f6b41d
Wolfang Torres <wolfang.torres@gmail.com>
2023-02-16 09:54:09 -0400

    update

--- src/sistema_indicadores/__init__.py
+++ src/sistema_indicadores/__init__.py
@@ -6,9 +6,4 @@ Application starting point
 # Standard Library
 import importlib.metadata
 
-DISTRIBUTION_METADATA = dict(importlib.metadata.metadata("sistema-indicadores"))
-
-print("hello world!")
-print("Data:")
-for key, val in DISTRIBUTION_METADATA.items():
-    print(f"\t{key}\t= {val}")
+DISTRIBUTION_METADATA = importlib.metadata.metadata("sistema-indicadores")
